1c630daa2f2484012ac362909ab9706c046da734
Paul Edwards <paedwar@microsoft.com>
2017-09-01 12:55:28 +0100

    Writing output to a file

--- allreduce/allreduce.sh
+++ allreduce/allreduce.sh
@@ -10,7 +10,7 @@ if [ "$NAME_TAG" != "" ]; then
 	NAME_TAG=_$NAME_TAG
 fi
 
-echo "NPROCS_LIST = $NPROCS_LIST"
+echo "NPROCS      = $NPROCS"
 echo "PPN_LIST    = $PPN_LIST"
 echo "NITER       = $NITER"
 echo "NAME_TAG    = $NAME_TAG"
@@ -24,18 +24,18 @@ for PPN in $PPN_LIST; do
 	mpirun -np $NP -ppn $PPN -hostfile $HOME/bin/hostlisti $AZURE_MPI_FLAGS $MPI_FLAGS IMB-MPI1 Allreduce -iter ${NITER} -npmin $NP -msglog 3:4 -time 1000000 2>&1 | tee IMB_Allreduce_${NITER}_${NP}_${NPROCS}x${PPN}${NAME_TAG}.log
 done
 
-OUTFILE=allreduce${NAME_TAG}_$(date +"%Y-%m-%d_%H-%M-%S").csv
+OUTFILE=allreduce_${NPROCS}_${NAME_TAG}_$(date +"%Y-%m-%d_%H-%M-%S").csv
 for MSG_SZ in 8 16; do
-	echo "MSG_SZ=${MSG_SZ}"
-	echo -n "NPROCS/PPN"
+	echo "MSG_SZ=${MSG_SZ}" > $OUTFILE
+	echo -n "NPROCS/PPN" >> $OUTFILE
 	for PPN in $PPN_LIST; do
-		echo -n " ${PPN}"
+		echo -n " ${PPN}" >> $OUTFILE
 	done
-	echo
-	echo -n "${NPROCS}"
+	echo >> $OUTFILE
+	echo -n "${NPROCS}" >> $OUTFILE
 	for PPN in $PPN_LIST; do
 		NP=$(bc <<< "$NPROCS * $PPN")
-		echo -n " $(grep -E " $MSG_SZ[ ]+$NITER " IMB_Allreduce_${NITER}_${NP}_${NPROCS}x${PPN}${NAME_TAG}.log | sed 's/  */ /g' | cut -d' ' -f 6)"
+		echo -n " $(grep -E " $MSG_SZ[ ]+$NITER " IMB_Allreduce_${NITER}_${NP}_${NPROCS}x${PPN}${NAME_TAG}.log | sed 's/  */ /g' | cut -d' ' -f 6)" >> $OUTFILE
 	done
 	echo
 done
