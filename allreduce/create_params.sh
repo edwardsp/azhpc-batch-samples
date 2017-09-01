5ef67b2caca138ea5794a88d86071064cff033b5
Paul Edwards <paedwar@microsoft.com>
2017-09-01 12:29:37 +0100

    Create the json parameter file

---
+++ allreduce/create_params.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+OUTPUT_STORAGE=$1
+NPROCS_LIST=$2
+PPN_LIST=$3
+NITER=$4
+NAME_TAG=$5
+MPI_FLAGS=$6
+
+cat << EOF
+{
+	"id":"$(uuidgen)",
+	"commandLine": "sudo -E -u _azbatch bash \$AZ_BATCH_TASK_WORKING_DIR/allreduce.sh '$2' '$3' '$4' '$5' '$6'",
+	"resourceFiles": [ 
+		{
+		  "blobSource":"https://raw.githubusercontent.com/edwardsp/azhpc-batch-samples/master/allreduce/allreduce.sh",
+		  "filePath":"allreduce.sh"
+		}  
+	],
+	"outputFiles" : [
+		{
+		"filePattern" : "\$AZ_BATCH_TASK_WORKING_DIR/*.csv",
+		"destination" : {
+			"container" : {
+				"path" : "allreduce-results",
+				"containerUrl": "$1"
+			}
+		},
+		"uploadOptions" : {
+			"uploadCondition" : "taskCompletion"
+			}
+		}
+	],	
+	"multiInstanceSettings":
+	{
+		"numberOfInstances" : 2,
+		"coordinationCommandLine" : "hostname",
+		"commonResourceFiles": [ ]
+	},
+	"userIdentity": {
+		"autoUser": {
+			"scope":"pool",
+			"elevationLevel":"admin"
+		}
+	}
+}
+EOF
